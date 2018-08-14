caf9d391fb652fe5e0ffb68c98f6a0053be17578
Emmett Keyser <emkeyser@gmail.com>
2018-08-14 09:24:21 -0700

    init

---
+++ poc-push-notification/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  poc-push-notification
+//
+//  Created by Emmett Keyser on 8/13/18.
+//  Copyright © 2018 ekeyser.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
